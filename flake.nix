fa8b17210e0b3e3ab0de5bf0f77c3d7d2ff613ef
tsandrini <tomas.sandrini@seznam.cz>
2024-03-04 15:16:23 +0100

    feat(flake): update inputs

--- flake.nix
+++ flake.nix
@@ -17,6 +17,19 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org/"
+      "https://devenv.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  };
+
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = with inputs; [treefmt-nix.flakeModule devenv.flakeModule];
