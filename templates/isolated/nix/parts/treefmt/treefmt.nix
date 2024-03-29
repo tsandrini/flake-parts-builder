c6c06e7892af241b62dbf160a17e3f91661c626d
tsandrini <tomas.sandrini@seznam.cz>
2024-03-29 15:48:38 +0100

    feat(devenv): update to v1 & add examples & remove impurities

--- templates/isolated/nix/parts/treefmt/treefmt.nix
+++ templates/isolated/nix/parts/treefmt/treefmt.nix
@@ -1,6 +1,14 @@
 # --- nix/parts/treefmt/treefmt.nix
 { pkgs, projectPath, ... }:
 {
+  # treefmt is a formatting tool that saves you time: it provides
+  # developers with a universal way to trigger all formatters needed for the
+  # project in one place.
+  # For more information refer to
+  #
+  # - https://numtide.github.io/treefmt/
+  # - https://github.com/numtide/treefmt-nix
+
   package = pkgs.treefmt;
   flakeCheck = true;
   flakeFormatter = true;
