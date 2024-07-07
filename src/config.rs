69d5d1dee303decb2c4599f601eef7144ae5d3b3
tsandrini <tomas.sandrini@seznam.cz>
2024-07-07 22:47:00 +0200

    fix(builder): fix flake.nix template rendering

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 pub static FLAKE_TEMPLATE: &'static str = include_str!("assets/flake.nix.template");
-pub static FLAKE_INPUTS_TEMPLATE: &'static str = include_str!("assets/flake.nix.template");
+pub static FLAKE_INPUTS_TEMPLATE: &'static str = include_str!("assets/flake-inputs.nix.template");
 pub static META_FILE: &'static str = "meta.nix";
 pub static NAMEPLACEHOLDER: &'static str = "NAMEPLACEHOLDER";
-pub static BASE_PARTS_DERIVATION: &'static str = "flake-parts";
+pub static BASE_DERIVATION_NAME: &'static str = "flake-parts";
 pub static SELF_FLAKE_URI: &'static str = "."; // TODO only for dev
                                                // static SELF_FLAKE_URI: &'static str = "github:tsandrini/flake-parts-builder";
