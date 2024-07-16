091b18a131f3c35fc1e56a5998d107a84be25228
tsandrini <tomas.sandrini@seznam.cz>
2024-07-16 11:40:54 +0200

    fix(builder): update temporarily SELF_FLAKE_URI

--- src/config.rs
+++ src/config.rs
@@ -3,5 +3,4 @@ pub static FLAKE_INPUTS_TEMPLATE: &'static str = include_str!("assets/flake-inpu
 pub static META_FILE: &'static str = "meta.nix";
 pub static NAMEPLACEHOLDER: &'static str = "NAMEPLACEHOLDER";
 pub static BASE_DERIVATION_NAME: &'static str = "flake-parts";
-pub static SELF_FLAKE_URI: &'static str = "."; // TODO only for dev
-                                               // static SELF_FLAKE_URI: &'static str = "github:tsandrini/flake-parts-builder";
+pub static SELF_FLAKE_URI: &'static str = "github:tsandrini/flake-parts-builder/v1";
